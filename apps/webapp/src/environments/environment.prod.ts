cbf7d87dcc9b74749364b14133a31dce1b19c1cb
FoxSGR <foxsgrno1@gmail.com>
2023-02-09 17:18:26 +0000

    chore: added resources to gitignore and environment file

--- apps/webapp/src/environments/environment.prod.ts
+++ apps/webapp/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080/api',
+  production: true,
+  api: 'http://localhost:3333/api',
+  resources: 'http://localhost:3333/resources',
 };
