0eea46bdb9919d1fb5f32d3d1a78d1ed643c7caa
FoxSGR <foxsgrno1@gmail.com>
2023-02-09 17:18:26 +0000

    fix: android build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'webapp',
-  webDir: 'www',
+  webDir: 'dist/apps/webapp',
   bundledWebRuntime: false
 };
 
