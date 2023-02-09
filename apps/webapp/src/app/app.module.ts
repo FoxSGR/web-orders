6127a6897b115e04bd96dcc102e3515766404d7f
FoxSGR <foxsgrno1@gmail.com>
2023-02-09 17:18:12 +0000

    refactor: added ionic

--- apps/webapp/src/app/app.module.ts
+++ apps/webapp/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, IonicModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
 })
